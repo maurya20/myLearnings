0bd5b6daa4fed6fa891a56300101c9b4082fa571
maurya20 <mukesh.ice17@gmail.com>
2020-07-07 16:38:05 +0530

    Add more field is working now.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+var z = document.getElementById("container");
 var x = document.getElementById("myBtn");
 x.addEventListener("click", myFunction);
 function myFunction() {
-  var y = document.forms["select"];
-  document.getElementById("demo").innerHTML += "Clicked!<br>";
+  var y = z.getElementsByTagName("form");
+  document.getElementById("demo").innerHTML += y[0].innerHTML;
 }
