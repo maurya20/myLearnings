e9f7bf599911c0344aed30cf1430629e3bd3abc6
maurya20 <mukesh.ice17@gmail.com>
2020-07-09 12:25:27 +0530

    Implementing AJAX on local server for AJAX code testing and it works.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-function myFunction() {
-  var d = new Date();
-  document.getElementById("demo").innerHTML = d.toLocaleTimeString() +"  " +d.getMilliseconds();
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "http://127.0.0.1:5500/image/stp.html", true);
+  xhttp.send();
 }
