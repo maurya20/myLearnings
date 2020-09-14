6225192091454d404295a10d460b5dcf57577b08
maurya20 <mukesh.ice17@gmail.com>
2020-09-14 17:57:06 +0530

    backtics with dollor sign in ES6 implemented.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,9 @@
-const cupcake = document.querySelector('.cupcake');
-const container = document.querySelector('.orders')
+function Me(){
+    const name = 'Mukesh'
+    const age = 26
+    const college = 'GCET'
+    document.getElementById('me').innerHTML = (`Hi there! my name is ${name}, I am ${age} years old and my college name was ${college}`)
 
-const cupcakes = [
- {id:1,flavor:'Strawberry'},
- {id:2,flavor:'Apple'},
- {id:3,flavor:'Banana'}
-]
-
-let randomCupcake;
-let orderNumber = 0;
-const baseAmount = 5;
-let finalAmount;
-const dicountID = 2;
-const dicountAmount = 3;
-let message
-
-cupcake.addEventListener("click",function(){
- // Select a random cupcake
- randomCupcake = Math.floor(Math.random() * cupcakes.length) + 0;
-
- // Increment the order number, we get a different order with each cupcake.
- orderNumber += 1;
-
- // Depending on the "dicountID" variable, this cupcake is on sale or not.
- finalAmount = dicountID == cupcakes[randomCupcake].id ? (baseAmount -dicountAmount ):baseAmount;
-
- // Create the message
- message = '<div>- You won a ' + cupcakes[randomCupcake].flavor + ' for ' + finalAmount + '$ , your order # is '+ orderNumber + '</div>'
- container.insertAdjacentHTML('beforeend',message)
-
-});
+    console.log(`Hi there! my name is ${name}, I am ${age} years old and my college name was ${college}`)
+}
+Me();
