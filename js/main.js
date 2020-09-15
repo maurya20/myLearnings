009789e0d21a03653d04ce4a4140e3e29d54dda8
maurya20 <mukesh.ice17@gmail.com>
2020-09-15 11:06:53 +0530

    insertAdjacentHTML('beforeend', template) implemented

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-
-    const user = {
-      name: "Mukesh",
-      age: 26,
-      college: "GCET",
-    };
-    document.getElementById(
-      "me"
-    ).innerHTML = `Hi there! my name is ${user.name}, I am ${user.age} years old and my college name was ${user.college}`;
-
-    console.log(
-      `Hi there! my name is ${user.name}, I am ${user.age} years old and my college name was ${user.college}`
-    );
+const list = document.querySelector('.list')
+function addUser(firstName, lastName){
+    const template= `
+    <div class="user">
+    <p>${firstName} ${lastName} </p>
+    </div>
+    `
+    list.insertAdjacentHTML('beforeend', template)
+}
+addUser("Mukesh","Maurya")
+addUser("Mukesh","Maurya")
+addUser("Banti ","Maurya")
