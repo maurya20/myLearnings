a8d26b58b4385f56629c7ef1b93d98d692152465
maurya20 <mukesh.ice17@gmail.com>
2020-09-15 16:45:27 +0530

    map method implemented.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
-const products = [
-    {name:'Iphone',price:200},
-    {name:'Motorola',price:70},
-    {name:'Samsung',price:150},
-    {name:'Sony',price:98},
-    {name:'Windows pone',price:10}
- ]
- let template = ''
- products.forEach(function(x){
-     function discount(){
-         if(x.price<100){
-         return `<span> On sale !! </span>`
-         }
-         else {
-            return `<div> Hurry up !! </div>`
-     }
-    }
-     template+=`<div class="product">
-     <h1> ${x.name}</h1>
-     ${discount()}
-     <strong>Price: $ ${x.price}</strong>
-     </div>`
- })
-
-document.body.insertAdjacentHTML("afterbegin", template)
+const arr = [1, 2, 3, 4]
+const newArray = arr.map(function(i){
+    return i*10
+})
+console.log(newArray)
+document.querySelector('.product').innerHTML=newArray[2]
