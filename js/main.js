a6d9431e10170f9f4548462140eab367f0972ccb
maurya20 <mukesh.ice17@gmail.com>
2020-09-15 15:37:42 +0530

    Total purchase price using for each with cleaner code.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const student = [ 'aniket','ankit','himanshu','gotom','shilpa','vinay']
-student.forEach(function(x){
-    console.log(x)
-}
-)
+const purchase = [
+    {quantity:2, amount:100},
+    {quantity:5, amount:100},
+    {quantity:10, amount:100}
+]
+let total = 0
+purchase.forEach(function(item){
+    total+= item.quantity*item.amount
+})
+console.log(total)
+document.querySelector(".list").innerHTML = `Total price for your purchase is Rs. ${total}`
