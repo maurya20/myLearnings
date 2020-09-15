b63019f15399506b060b39eb3e619aebf4e588b0
maurya20 <mukesh.ice17@gmail.com>
2020-09-15 10:31:47 +0530

    ES 6 object rendering using backstics and $-

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-function Me(){
-    const name = 'Mukesh'
-    const age = 26
-    const college = 'GCET'
-    document.getElementById('me').innerHTML = (`Hi there! my name is ${name}, I am ${age} years old and my college name was ${college}`)
 
-    console.log(`Hi there! my name is ${name}, I am ${age} years old and my college name was ${college}`)
-}
-Me();
+    const user = {
+      name: "Mukesh",
+      age: 26,
+      college: "GCET",
+    };
+    document.getElementById(
+      "me"
+    ).innerHTML = `Hi there! my name is ${user.name}, I am ${user.age} years old and my college name was ${user.college}`;
+
+    console.log(
+      `Hi there! my name is ${user.name}, I am ${user.age} years old and my college name was ${user.college}`
+    );
