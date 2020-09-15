e6269144e4d4d2c2fdffb888e44b79fdf18c383f
maurya20 <mukesh.ice17@gmail.com>
2020-09-15 17:04:52 +0530

    Option selector with ES6 map and forEach methods.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-const arr = [1, 2, 3, 4]
-const newArray = arr.map(function(i){
-    return i*10
-})
-console.log(newArray)
-document.querySelector('.product').innerHTML=newArray[2]
+const paintings = [
+    {name:'Mona lisa',width:200,height:200},
+    {name:'The scream',width:400,height:600},
+    {name:'The last supper',width:600,height:700}
+ ]
+ const list = paintings.map(function(i){
+     return i.name
+ })
+ console.log(list)
+ list.forEach(function(name){
+     const select = document.querySelector('select')
+     select.insertAdjacentHTML('afterbegin', `<option value='${name}'>${name}</option>`)
+ })
