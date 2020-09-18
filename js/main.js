a8b3f43cef0f90132d940c61922cf9637d7646bd
maurya20 <mukesh.ice17@gmail.com>
2020-09-18 16:36:10 +0530

    Es6 fetch . then promises implemented.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-//var distances = [distance1,distance2,distance3]
-let total = 0;
-const totalDistance = (...totalDistance) =>{
-    return total = totalDistance[0]+totalDistance[1]+totalDistance[2]
-    
- }
- totalDistance(200,1100,200)
- console.log(total)
+const url = 'http://jsonplaceholder097yh.typicode.com/posts/1'
+fetch(url)
+.then(response=>response.json())
+.then(data=> console.log(data))
+.catch(error => console.log("What the helll is this crap", error))
+
