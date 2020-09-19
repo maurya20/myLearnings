6186232f2bb5d6dfedf563fa88f8a37821551578
maurya20 <mukesh.ice17@gmail.com>
2020-09-19 09:26:48 +0530

    ES 6 module import export implemented.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+export const user = {
+    name:"Mukesh",
+    age:26,
+    adr: {
+        city:"Delhi",
+        pin: 110086
+    }
+}
+
+
+
+export const sum = 10
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-const url = 'http://jsonplaceholder097yh.typicode.com/posts/1'
-fetch(url)
-.then(response=>response.json())
-.then(data=> console.log(data))
-.catch(error => console.log("What the helll is this crap", error))
+import { user, sum } from "./app.js"
 
+console.log(user.adr.city)
+
+console.log(sum)
